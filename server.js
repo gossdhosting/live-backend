35ca0bf0f22148d745ff21f3bdc4ea599f1123da
Voltxt.io <gossdhosting@gmail.com>
2026-01-03 04:32:12 +0530

    first commit

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+app.set('trust proxy', 1); // Add this line
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
